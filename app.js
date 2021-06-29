cd2f2c7f68b71f558cdc37ddd99670f85bf456b5
saksham701 <iit2019022@iiita.ac.in>
2021-06-29 18:55:08 +0530

    updated app.js

--- app.js
+++ app.js
@@ -148,7 +148,7 @@ app.use('/', pages);
 
 
 //start server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port,  () => {
     console.log('listening on port ' + port);
 });
